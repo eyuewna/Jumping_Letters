e4408d4b3c12d7fa7d3b850f81235c205c0c5157
Euna Angeles <202210630@fit.edu.ph>
2024-03-13 20:57:53 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelectorAll(".matcha");
-
-container.forEach((item) => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("active");
-    });
-});
